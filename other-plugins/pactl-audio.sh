aa1b2f6db9f9e3c384df0fe3562208ac6c41a430
imgurbot12 <imgurbot12@gmail.com>
2024-03-28 18:08:12 -0700

    fix: remove debug line

--- other-plugins/pactl-audio.sh
+++ other-plugins/pactl-audio.sh
@@ -4,7 +4,6 @@ get_sinks() {
   sinks=`pactl list sinks | grep -e 'Sink #' -e 'Name: ' -e 'Description: ' | nl -s '>'`
   default=`pactl get-default-sink`
   for i in `seq 1 3 $(echo "$sinks" | wc -l)`; do
-    echo "$sinks" | grep "$i>"
     sink=`echo "$sinks" | grep "$i>" | cut -d '#' -f2`
     name=`echo "$sinks" | grep "$(expr $i + 1)>" | cut -d ':' -f2 | xargs echo -n`
     desc=`echo "$sinks" | grep "$(expr $i + 2)>" | cut -d ':' -f2 | xargs echo -n`
